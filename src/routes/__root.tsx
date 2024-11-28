d9699de65a04cdb50171372d5fc8029e8ab8614b
Jeremy Viallatt <shoo>
2024-11-28 19:19:24 +0100

    fix header / nav

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,21 +5,51 @@ import {
 } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { RouterContext } from "../main.tsx";
+import { useUserContext } from "../hooks/UserContext.tsx";
+import { useMemo } from "react";
+import { Text, Box, Flex, Image, Title } from "@mantine/core";
+import HomeImageUrl from "../assets/home.png";
 
 export const Route = createRootRouteWithContext<RouterContext>()({
-  component: () => (
-    <>
-      <nav className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Listes de cadeaux
-        </Link>
-        <Link to="/list-new" className="[&.active]:font-bold">
-          Ajouter une liste
-        </Link>
-      </nav>
-      <hr />
-      <Outlet />
-      <TanStackRouterDevtools />
-    </>
-  ),
+  component: () => {
+    const { isAuthenticated } = useUserContext();
+
+    const isLoggedIn = useMemo(
+      () => (
+        <Box component="nav" h={40} bg="red.4" color="green" pl="8">
+          <Flex gap="xl" align="center" h="inherit">
+            <Link
+              to="/"
+              style={{ textDecoration: "none" }}
+              activeProps={{ className: "underline" }}
+            >
+              <Text td="underline" size="lg" c="green.7" fw="bold">
+                Listes de cadeaux
+              </Text>
+            </Link>
+            <Link to="/list-new" style={{ textDecoration: "none" }}>
+              <Text td="underline" size="lg" c="green.7" fw="bold">
+                Ajouter une liste
+              </Text>
+            </Link>
+          </Flex>
+        </Box>
+      ),
+      [],
+    );
+
+    return (
+      <>
+        <Box h={100} bg="red" c="white">
+          <Flex align="center" gap="xl">
+            <Image src={HomeImageUrl} h={100} fit="contain" w="auto" />
+            <Title order={1}>Wrap It Up !</Title>
+          </Flex>
+        </Box>
+        {isAuthenticated && isLoggedIn}
+        <Outlet />
+        <TanStackRouterDevtools />
+      </>
+    );
+  },
 });
--- src/utils/routeUtils.ts
+++ src/utils/routeUtils.ts
@@ -2,7 +2,6 @@ import { RouterContext } from "../main.tsx";
 import { redirect } from "@tanstack/react-router";
 
 const isAuthenticated = (context: RouterContext) => {
-  console.log("toto", !context.auth?.isAuthenticated);
   if (!context.auth?.isAuthenticated) {
     throw redirect({
       to: "/login",
