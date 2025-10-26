b87591252ae0a7cdc4e586af8510d14594eae77f
Jeremy Viallatt <shoo>
2025-10-26 19:41:54 +0100

    fix ci

--- src/repository/FileRepository.ts
+++ src/repository/FileRepository.ts
@@ -4,7 +4,9 @@ const updateFile = (file: File) => {
   const storage = getStorage();
   const storageRef = ref(storage, file.name);
 
-  uploadBytes(storageRef, file).then((snapshot) => {
+  uploadBytes(storageRef, file).then((_snapshot) => {
     console.log("Uploaded a blob or file!");
   });
 };
+
+export { updateFile };
